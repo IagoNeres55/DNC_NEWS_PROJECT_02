2fa19c0e78d63618f87d92173ccc2d4cff32a668
IagoNeres55 <iagoneres90@gmail.com>
2024-10-03 18:00:43 -0300

    add cors

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import express from "express";
 import userRouter from "./src/routes/usuarios.routes.js";
 import noticiaRouter from "./src/routes/noticias.routes.js";
 
+import cors from "cors";
+
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use(userRouter);
